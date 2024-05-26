b276cb2ea71c85a583ed5e2ccc86254c84b5ffaa
Fernando <fernando_rendel892@hotmail.com>
2024-05-25 20:53:50 -0600

    feat(usuarios-controller):se camió el método Validator a privado, de lo contrario todos los métodos públicos a excepción del constructor, requieren un verbo http para que sagger los mapee.

--- Postres.Funciones/Usuarios/IUsuariosCommand.cs
+++ Postres.Funciones/Usuarios/IUsuariosCommand.cs
@@ -1,9 +1,4 @@
 ﻿using Postres.Infraestructura.APIServices;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Postres.Funciones.Usuarios
 {
