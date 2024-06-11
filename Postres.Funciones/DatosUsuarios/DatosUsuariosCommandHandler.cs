a8e4ef723553b5ed20c8167b022f7b72fb469612
Fernando <fernando_rendel892@hotmail.com>
2024-06-11 16:33:17 -0600

    feat(Postres-Funciones-DatosUsuarios):se añade configuración básica para datos de los usuarios

--- Postres.Funciones/DatosUsuarios/DatosUsuariosCommandHandler.cs
+++ Postres.Funciones/DatosUsuarios/DatosUsuariosCommandHandler.cs
@@ -1,17 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Postres.Domain;
 using Postres.Infraestructura.APIServices;
 
 namespace Postres.Funciones.DatosUsuarios
 {
     public class DatosUsuariosCommandHandler : IDatosUsuariosCommand
     {
+        private readonly PostresDBContext _postresDBContext;
+
+        public DatosUsuariosCommandHandler(PostresDBContext postresDBContext)
+        {
+            _postresDBContext = postresDBContext ?? throw new ArgumentException(nameof(postresDBContext));
+        }
+
         public Task<ResultAPI> DeleteDataUser(string usuario)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ResultAPI> GetAllDataUsers()
+        public async Task<ResultAPI> GetAllDataUsers()
         {
-            throw new NotImplementedException();
+            var allData = await _postresDBContext.DatosUsuarios.ToListAsync();
+
+            if (allData.Count == 0) return ResultAPI.Ok("No existe ningún registro de este usuario");
+
+            return ResultAPI.Ok(allData, "Detalles del usuario");
         }
 
         public Task<ResultAPI> GetDataUserByName(string usuario)
